1e757f730ca76df1ecabaa41301aa31b00c4da89
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-06-25 21:02:55 +0200

    Update Search.js
    Delete comments in Search.js

--- components/Search.js
+++ components/Search.js
@@ -1,45 +1,27 @@
-// search bar
-// it starts searching:
-// 1. when you press enter
-// 2. or when the length of the text you enter will be greater than 2 letters
-
 Search = React.createClass({
-  // ----------------------------SET INITIAL STATE -----------------------------
+
   getInitialState() {
     return {
       searchingText: ''
     };
   },
 
-  // --------------------------HANDLECAHNGE METHOD------------------------------
-// kazda zmiana generuje zdarzenie
-  handleChange: function(event) {    //? o co chodzi z ta funkacj
-    // wchodzimy do wartosci zdarzenia poprzez target.value
-    // target to jest info ktory zostal ruszony (gdzieto jhest w html)
-    var searchingText1 = event.target.value;  //??
-    // zaktualizowanie stanu komponentu
-    // klucz i wartosc
+  handleChange: function(event) {
+    var searchingText1 = event.target.value;
     this.setState({
       searchingText: searchingText1
     });
-    // the question is sent if it has more than 2 characters
     if (searchingText.length > 2) {
-      // searchingText -> our text in searching
-      // odpalanie tej funkcji z app z parametrem tekstem
       this.props.onSearch(searchingText);
     }
   },
-  // recognizes that the pressed key is enter and sends a message to the parent
-  //so that this one again starts the function that sends the query after the gifa
+
   handleKeyUp: function(event) {
-    // 13 it is code button ENTER
-    // if you press the key ENTER
     if (event.keyCode === 13) {
-      this.props.onSearch(this.state.searchingText);  //co robi ta funkcja //dlaczego this.state.a nie sam searchingText
-    }//gdzie tu jest ze wysyla wiadomosc do rodzica?a on jeszcze raz uruchomi funkcje wysylajac pytanie o gifa
+      this.props.onSearch(this.state.searchingText);
+    }
   },
 
-  // --------------------------------RENDER-------------------------------------
   render: function() {
 
     var styles = {
@@ -50,15 +32,11 @@ Search = React.createClass({
 
     return < input
       type = 'text'
-      // kiedy cos sie wklei w pasek wyszukiwarki nasluch
       onChange = {this.handleChange}
-      // listening for pressing the enter key(nasluch na eneter)
-      // kiedy kliknie sie cos na klawiaturze nasluch
       onKeyUp = {this.handleKeyUp}
       placeholder = 'Enter the search phrase here'
       style = {styles}
       value = {this.state.searchingText}
     />
-
   }
 });
