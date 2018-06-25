71ad4b522c4efa1dc143532b645ec7929d8acc01
Zszywka <32071894+Zszywka@users.noreply.github.com>
2018-06-26 00:37:44 +0200

    Update Gif.js
    Matched Gif.js

--- components/Gif.js
+++ components/Gif.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 var GIPHY_LOADING_URL = '/loader.gif';
-=======
-var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
->>>>>>> 4857a966cc90349ba07c467fc57d95738aa085fe
 
 var styles = {
   minHeight: '310px',
@@ -14,11 +10,8 @@ Gif = React.createClass({
     return this.props.sourceUrl || GIPHY_LOADING_URL;
   },
   render: function() {
-<<<<<<< HEAD
     var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
-=======
-    var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url; 
->>>>>>> 4857a966cc90349ba07c467fc57d95738aa085fe
+
     return(
       <div style = {styles}>
         <a href = {this.getUrl()} title = 'view this on giphy' target = 'new'>
